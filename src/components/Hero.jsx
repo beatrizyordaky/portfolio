30f5f1c0d274a0a66709a26774821f89b3423c12
Beatriz <beatriz.yordaky@quintoandar.com.br>
2025-06-27 20:03:05 -0300

    feat: add particle component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Particles from "./Particles";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div style={{ width: "100%", height: "600px", position: "relative" }}>
+      <Particles
+        particleColors={["#ffffff", "#ffffff"]}
+        particleCount={200}
+        particleSpread={10}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={true}
+        alphaParticles={false}
+        disableRotation={false}
+      />
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
